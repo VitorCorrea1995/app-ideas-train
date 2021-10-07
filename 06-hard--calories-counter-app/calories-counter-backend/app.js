32ad55ae9469c4a17ed9a8852ce43b21862e4827
Vitor Augusto Correa <augusto.vitor.correa@gmail.com>
2021-10-06 21:06:00 -0300

    Wrap setup of app

--- 06-hard--calories-counter-app/calories-counter-backend/app.js
+++ 06-hard--calories-counter-app/calories-counter-backend/app.js
@@ -1,28 +1,5 @@
-const HttpServer = require('./http-server');
 const logger = require('./util/log/logger');
 
-const os = require('os');
-const cluster = require('cluster');
+require('./load-servers')();
 
-const clusterWorkerSize = os.cpus().length;
-
-if (clusterWorkerSize > 1) {
-  if (cluster.isMaster) {
-    
-    for (let workerCount = 1; workerCount < clusterWorkerSize; workerCount++) {
-      logger.info(`Loading server...`);
-      cluster.fork({worker_id: workerCount});
-    }
-
-    cluster.on('exit', function(worker) {
-      logger.warn(`Worker ${worker.id} has exitted.`);
-      cluster.fork({worker_id: worker.id})
-    });
-  } else {
-    (new HttpServer()).start();
-    logger.info(`Server is running!`);
-  }
-} else {
-  (new HttpServer()).start();
-  logger.info(`Server is running!`);
-}
+logger.info(`App load successfully and started!! 🤟 🤟 🤟`);
